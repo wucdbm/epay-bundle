bfd80b34d370ccb8ca90d8c683156646659edadf
Martin Kirilov <wucdbm@gmail.com>
2015-12-11 02:56:48 +0200

    fix

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wucdbm\Bundle\EpayBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface {
+
+    public function getConfigTreeBuilder() {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('wucdbm_epay');
+
+        $rootNode
+            ->children()
+                ->scalarNode('client_options')
+                    ->isRequired()
+                ->end()
+                ->scalarNode('client_handler')
+                    ->isRequired()
+                ->end()
+            ->end();
+
+        exit('configuration :)');
+
+        return $treeBuilder;
+    }
+
+}
--- DependencyInjection/WucdbmEpayExtension.php
+++ DependencyInjection/WucdbmEpayExtension.php
@@ -10,6 +10,47 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 class WucdbmEpayExtension extends Extension {
 
     public function load(array $configs, ContainerBuilder $container) {
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $bag = $container->getParameterBag();
+
+        $bag->set('wucdbm_locale.config', $config);
+
+        $locales = $config['locales'];
+        $localesSimple = array_keys($locales);
+
+        $bag->set('wucdbm_locale.locales', $locales);
+        $bag->set('wucdbm_locale.locales_simple', $localesSimple);
+
+        $loader->load('services/managers.xml');
+        $loader->load('services/forms.xml');
+
+        if (isset($config['cookie_listener']) && $config['cookie_listener']['enabled']) {
+            $bag->set('wucdbm_locale.cookie_listener', $config['cookie_listener']);
+            $loader->load('services/listener/cookie.xml');
+        }
+
+        if (isset($config['disabled_locale_redirect_listener']) && $config['cookie_listener']['enabled']) {
+            $bag->set('wucdbm_locale.disabled_locale_redirect_listener', $config['disabled_locale_redirect_listener']);
+            $loader->load('services/listener/disabled_locale_redirect.xml');
+        }
+
+        if (isset($config['jms_integration']) && $config['jms_integration']) {
+            $bag->set('jms_translation.locales', $localesSimple);
+        }
+
+        $bag->set('wucdbm_locale.locales_enabled.routing', implode('|', $localesSimple));
+
+
+
+
+
         $config = array();
         foreach ($configs as $subConfig) {
             $config = array_merge($config, $subConfig);
@@ -19,6 +60,11 @@ class WucdbmEpayExtension extends Extension {
             new FileLocator(__DIR__.'/../Resources/config')
         );
         $loader->load('services.xml');
+
+
+
+
+
     }
 
     public function getXsdValidationBasePath() {
